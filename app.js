2d264bacd418af7b23a4a8abd2485f20d16b36b7
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-03-19 01:04:01 +0300

    http get request

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const app = express();
 
+const products = [
+  { id: 1, name: "iphone12", price: 20000 },
+  { id: 2, name: "iphone13", price: 30000 },
+  { id: 3, name: "iphone14", price: 40000 },
+];
+
 app.get("/", (req, res) => {
-  res.send("popüler ürün");
+  res.send(products[0]);
+});
+
+app.get("/api/products", (req, res) => {
+  res.send(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  console.log(req.params);
+  console.log(req.query);
+  const product = products.find((p) => p.id == req.params.id);
+
+  if (!product) {
+    res.status(404).send("Aradığınız ürün bulunamadı.");
+  }
+  res.send(product);
 });
 
 app.listen(3000, () => {
