1944acafbf3cd86097b818fc7ee26a8cb51e8cca
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-03-20 00:44:43 +0300

    kayıt ekleme (save data)

--- app.js
+++ app.js
@@ -18,16 +18,20 @@ app.use(
 app.use("/api/products", products);
 app.use("/", home);
 
-mongoose
-  .connect(
-    "mongodb+srv://alperenaktug:Moriarty1579.843@cluster0.ufy6lbx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => {
+const username = "alperenaktug";
+const password = "Moriarty1579.843";
+const database = "shopdb";
+
+(async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${username}:${password}@cluster0.ufy6lbx.mongodb.net/${database}?retryWrites=true&w=majority&appName=Cluster0`
+    );
     console.log("mongodb bağlantısı kuruldu.");
-  })
-  .catch((err) => {
+  } catch (error) {
     console.log(err);
-  });
+  }
+})();
 
 const productSchema = mongoose.Schema({
   name: String,
@@ -40,8 +44,29 @@ const productSchema = mongoose.Schema({
   },
   isActive: Boolean,
 });
-
+// model oluşturma
 const Product = mongoose.model("Product", productSchema);
+
+//nesne oluşturma
+// kayıt ekleme
+const prd = new Product({
+  name: "iphone 14",
+  price: "30000",
+  description: "iyi telefon",
+  imageURL: "1.jpeg",
+  isActive: true,
+});
+
+async function saveProduct() {
+  try {
+    const result = await prd.save();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+saveProduct();
 
 app.listen(3005, () => {
   console.log("listening on port 3000");
